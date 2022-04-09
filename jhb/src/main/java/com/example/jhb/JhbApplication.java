2b3531144f36e1c4f70b8692a03c08a4b274d69d
jianghaibo <jiang.haibo@ustcinfo.com>
2022-04-09 15:11:40 +0800

    “前端代码初始化”

---
+++ jhb/src/main/java/com/example/jhb/JhbApplication.java
@@ -0,0 +1,13 @@
+package com.example.jhb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhbApplication.class, args);
+    }
+
+}
---
+++ jhb/src/main/java/com/example/jhb/commom/ResultCode.java
@@ -0,0 +1,8 @@
+package com.example.jhb.commom;
+
+public interface ResultCode {
+
+    public static Integer SUCCESS = 200; //成功
+
+    public static Integer ERROR = 500; //失败
+}
